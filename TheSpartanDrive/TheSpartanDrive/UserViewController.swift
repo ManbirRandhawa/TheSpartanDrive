bde635646c597ee8633f46930a1567c03e77f785
ManbirRandhawa <mrandhawa27@gmail.com>
2016-05-03 20:41:53 -0700

    Password field updated to hide the password

--- TheSpartanDrive/TheSpartanDrive/UserViewController.swift
+++ TheSpartanDrive/TheSpartanDrive/UserViewController.swift
@@ -14,11 +14,8 @@ class UserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            print("Object has been saved.")
-        }
+        
+        
     }
     @IBAction func logUserOut(sender: AnyObject) {
         PFUser.logOut()
